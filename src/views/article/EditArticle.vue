c08b03a3bc582dbc0ac4282939f346abcfd22344
shenzhou-6 <yizmao.me@gmail.com>
2019-03-20 16:03:12 +0800

    add simplemarkdown

--- src/views/article/EditArticle.vue
+++ src/views/article/EditArticle.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>EDIT</div>
+    <textarea></textarea>
 </template>
 
 <script>
+    import 'font-awesome/css/font-awesome.min.css'
+    import 'simplemde/dist/simplemde.min.css'
+    import SimpleMDE from 'simplemde'
     export default {
-        name: "EditArticle"
+        name: "EditArticle",
+        data(){
+            return{
+                markdown:null,
+            }
+        },
+        mounted() {
+            this.markdown = new SimpleMDE({
+                element:document.getElementById("markdown"),
+                autosave:{
+                    enabled:true,
+                    uniqueId:'articleEdit'
+                },
+                toolbar:false
+            })
+        }
     }
 </script>
 
